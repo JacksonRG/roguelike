a491510dc29dad689c6a69bc1ea86624a33b6922
Jackson <jackson@ganymede.home>
2020-12-02 13:33:56 -0600

    Added rooms, and dynamically place player in center of first room

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,96 @@
+#![allow(dead_code)]
 use tcod::colors::*;
 use tcod::console::*;
 
+//mod rooms;
 
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
 
+const MAP_WIDTH: i32 = 80;
+const MAP_HEIGHT: i32 = 45;
+
 const LIMIT_FPS: i32 = 20;
 
+const COLOR_DARK_WALL: Color = Color{r: 0, g: 0, b: 100};
+const COLOR_DARK_GROUND: Color = Color{
+	r: 50,
+	g: 50,
+	b: 150,
+};
+
+#[derive(Clone, Copy, Debug)]
+struct Tile{
+	blocked: bool,
+	block_sight: bool,
+}
+
+impl Tile{
+	pub fn empty() -> Self {
+		Tile {
+			blocked: false,
+			block_sight: false,
+		}
+	}
+	pub fn wall() -> Self {
+		Tile {
+			blocked: true,
+			block_sight: true,
+		}
+	}
+}
+
+type Map = Vec<Vec<Tile>>;
+
+struct Game{
+	map: Map,
+}
+
+fn make_map(sx: &mut i32, sy: &mut i32) -> Map {
+	let mut map = vec![vec![Tile::wall(); MAP_HEIGHT as usize];
+	MAP_WIDTH as usize];
+	map[30][22] = Tile::wall();
+	map[50][22] = Tile::wall();
+	let room1 = Rect::new(20,15, 10, 15);
+	let room2 = Rect::new(50,15, 10, 15);
+	*sx = (room1.x2 - room1.x1) / 2 + room1.x1;
+	*sy = (room1.y2 - room1.y1) / 2 + room1.y1;
+	create_room(room1, &mut map);
+	create_room(room2, &mut map);
+	map
+}
+
+fn render_all(tcod: &mut Tcod, game: &Game, objects: &[Object]){
+	for object in objects {
+		object.draw(&mut tcod.con);
+	}
+
+	for y in 0..MAP_HEIGHT {
+		for x in 0..MAP_WIDTH {
+			let wall = game.map[x as usize][y as usize].block_sight;
+			if wall {
+				tcod.con
+					.set_char_background(x,y,COLOR_DARK_WALL,
+						BackgroundFlag::Set);
+			} else {
+				tcod.con
+					.set_char_background(x,y, COLOR_DARK_GROUND,
+						BackgroundFlag::Set);
+			}
+		}
+	}
+
+	blit(&tcod.con,
+		(0,0),
+		(MAP_WIDTH, MAP_HEIGHT),
+		&mut tcod.root,
+		(0,0),
+		1.0,
+		1.0,
+	);
+}
+
+#[derive(Debug)]
 struct Object {
 	x: i32,
 	y: i32,
@@ -19,9 +103,11 @@ impl Object {
 		Object { x, y, char, color }
 	}
 
-	pub fn move_by(&mut self, dx: i32, dy: i32) {
-		self.x += dx;
-		self.y += dy;
+	pub fn move_by(&mut self, dx: i32, dy: i32, game: &Game) {
+		if !game.map[(self.x+dx) as usize][(self.y+dy) as usize].blocked {
+			self.x += dx;
+			self.y += dy;
+		}
 	}
 
 	pub fn draw(&self, con: &mut dyn Console) {
@@ -37,8 +123,9 @@ struct Tcod {
 
 fn handle_keys(
 	tcod: &mut Tcod,
-	player_x: &mut i32,
-	player_y: &mut i32) -> bool {
+	game: &Game,
+	player: &mut Object
+	) -> bool {
 
 	use tcod::input::Key;
 	use tcod::input::KeyCode::*;
@@ -54,13 +141,39 @@ fn handle_keys(
 			tcod.root.set_fullscreen(!fullscreen);
 		}
 		Key { code: Escape, ..} => return true,
-		Key { code: Up, .. } => *player_y -=1,
-		Key { code: Down, .. } => *player_y +=1,
-		Key { code: Left, .. } => *player_x -=1,
-		Key { code: Right, .. } => *player_x +=1,
+		Key { code: Up, .. } => player.move_by(0,-1, game),
+		Key { code: Down, .. } => player.move_by(0,1, game),
+		Key { code: Left, .. } => player.move_by(-1,0, game),
+		Key { code: Right, .. } => player.move_by(1,0, game),
 		_ => {}
 	}
 	false
+}
+
+struct Rect{
+	x1: i32,
+	y1: i32,
+	x2: i32,
+	y2: i32,
+}
+
+impl Rect {
+	pub fn new(x: i32, y: i32, w: i32, h: i32) -> Self {
+		Rect {
+			x1: x,
+			y1: y,
+			x2: x + w,
+			y2: y + h,
+		}
+	}
+}
+
+fn create_room(room: Rect, map: &mut Map) {
+	for x in (room.x1 + 1)..room.x2{
+		for y in (room.y1 + 1)..room.y2 {
+			map[x as usize][y as usize] = Tile::empty();
+		}
+	}
 }
 
 fn main() {
@@ -70,32 +183,45 @@ fn main() {
         .size(SCREEN_WIDTH, SCREEN_HEIGHT)
         .title("Rust/libtcod tutorial")
         .init();
-	let con = Offscreen::new(SCREEN_WIDTH, SCREEN_HEIGHT);
+	let con = Offscreen::new(MAP_WIDTH, MAP_HEIGHT);
     let mut tcod = Tcod { root, con };
     tcod::system::set_fps(LIMIT_FPS);
 
-    let mut player_x = SCREEN_WIDTH / 2;
-    let mut player_y = SCREEN_HEIGHT / 2;
+	//Player start point is set in map creation.
+    let mut player_start_x: i32 = 0;
+    let mut player_start_y: i32 = 0;
+
+	let game = Game{
+		map: make_map(&mut player_start_x, &mut player_start_y),
+	};
+	//create player object
+	let player = Object::new(player_start_x, player_start_y, '@', WHITE);
+	//create npc object
+	let npc = Object::new(player_start_x-5, player_start_y, '@', YELLOW);
+	// list of objects
+	let mut objects = [player, npc];
+
     while !tcod.root.window_closed() {
-        tcod.con.set_default_foreground(WHITE);
         tcod.con.clear();
-		tcod.con
-			.put_char(player_x, player_y, '@', BackgroundFlag::None);
-		blit(&tcod.con,
-			(0,0),
-			(SCREEN_WIDTH, SCREEN_HEIGHT),
-			&mut tcod.root,
-			(0,0),
-			1.0,
-			1.0,
-		);
+		for object in &objects{
+			object.draw(&mut tcod.con);
+		}
+
+		//render_all(tcod: &mut Tcod, game: &Game, objects: &objects);
+		render_all(&mut tcod, &game, &objects);
         tcod.root.flush();
-        //tcod.root.wait_for_keypress(true);
 
-
-		let exit = handle_keys(&mut tcod, &mut player_x, &mut player_y);
+		let player = &mut objects[0];
+		let exit = handle_keys(&mut tcod, &game, player);
+		//player passed normally, because it's already a mut
+		//borrow of objects[0]
 		if exit {
 			break;
 		}
     }
+}
+
+#[test]
+fn usize_max(){
+	println!("{}", usize::MAX);
 }
---
+++ src/rooms.rs
@@ -0,0 +1,27 @@
+mod rooms {
+	struct Rect{
+		x1: i32,
+		y1: i32,
+		x2: i32,
+		y2: i32,
+	}
+
+	impl Rect {
+		pub fn new(x: i32, y: i32, w: i32, h: i32) -> Self {
+			Rect {
+				x1: x,
+				y1: y,
+				x2: x + w,
+				y2: y + h,
+			}
+		}
+	}
+
+	fn create_room(room: Rect, map: &mut Map) {
+		for x in (room.x1 + 1)..room.x2{
+			for y in (room.y1 + 1)..room.y2 {
+				map[x as usize][y as usize] = Tile::empty();
+			}
+		}
+	}
+}
